f08c7a4727f151b5f92f58e21fe988378b7912f3
Nicolás Umpierrez <nicolas.umpierrez@gosocket.net>
2022-10-29 17:58:34 -0300

    map responses to json

--- src/main/java/com/example/neo4j/Controller/CrimeController.java
+++ src/main/java/com/example/neo4j/Controller/CrimeController.java
@@ -2,14 +2,10 @@ package com.example.neo4j.Controller;
 
 
 import com.example.neo4j.Service.CrimeService;
-import com.example.neo4j.Service.NeoService;
-import com.example.neo4j.model.Movie;
-import com.example.neo4j.model.Person;
-import com.example.neo4j.repository.MovieRepository;
 import lombok.AllArgsConstructor;
 import org.neo4j.driver.Record;
-import org.neo4j.driver.internal.shaded.reactor.core.publisher.Flux;
-import org.neo4j.driver.internal.shaded.reactor.core.publisher.Mono;
+import org.neo4j.driver.Value;
+import org.neo4j.driver.util.Pair;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,12 +21,12 @@ public class CrimeController
     private CrimeService crimeService;
 
     @GetMapping(path ="/address/{address}/postcode/{postcode}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Record> getCrimeByAddress(@PathVariable String address, @PathVariable String postcode){
-        return crimeService.getCrimeByAddress(address,postcode);
+    public ResponseEntity<String> getCrimeByAddress(@PathVariable String address, @PathVariable String postcode){
+        return ResponseEntity.ok(crimeService.getCrimeByAddress(address,postcode));
     }
 
     @GetMapping(path ="/dangerousPersonsByFriend/{frinedId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Record> getDengerousPersonsByFriend(@PathVariable String frinedId){
+    public String getDengerousPersonsByFriend(@PathVariable String frinedId){
         return crimeService.getDengerousPersonsByFriend(frinedId);
     }
 
--- src/main/java/com/example/neo4j/Service/CrimeService.java
+++ src/main/java/com/example/neo4j/Service/CrimeService.java
@@ -1,17 +1,15 @@
 package com.example.neo4j.Service;
 
 
-import com.example.neo4j.model.Movie;
-import com.example.neo4j.model.Person;
-import com.example.neo4j.repository.MovieRepository;
-import com.example.neo4j.repository.PersonRepository;
+import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
 import org.neo4j.driver.*;
 import org.neo4j.driver.Record;
+import org.neo4j.driver.util.Pair;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -20,18 +18,27 @@ public
 class CrimeService {
     private final Driver driver;
 
-    public List<Record> getCrimeByAddress(String address, String postcode) {
+    public String getCrimeByAddress(String address, String postcode) {
         try (Session session = driver.session()) {
-            String query = String.format("MATCH (l:Location {address: '%s', postcode: '%s' WITH point(l) AS corrie MATCH (x:Location)-[:HAS_POSTCODE]->(p:PostCode),(x)<-[:OCCURRED_AT]-(c:Crime) WITH x, p, c, distance(point(x), corrie) AS distance WHERE distance < 500 RETURN x.address AS address, p.code AS postcode, count(c) AS crime_total, collect(distinct(c.type)) AS crime_type, distance ORDER BY distance LIMIT 10", address, postcode);
-            return session.run(query).stream().collect(Collectors.toList());
+            String query = String.format("MATCH (l:Location {address: '%s', postcode: '%s'}) WITH point(l) AS corrie MATCH (x:Location)-[:HAS_POSTCODE]->(p:PostCode),(x)<-[:OCCURRED_AT]-(c:Crime) WITH x, p, c, point.distance(point(x), corrie) AS distance WHERE distance < 500 RETURN x.address AS address, p.code AS postcode, count(c) AS crime_total, collect(distinct(c.type)) AS crime_type, distance ORDER BY distance LIMIT 10", address, postcode);
+            return mapResponseToString(session, query);
         }
     }
 
-    public List<Record> getDengerousPersonsByFriend(String dni) {
+    public String getDengerousPersonsByFriend(String dni) {
         try (Session session = driver.session()) {
             String query = String.format("MATCH (anne:Person {nhs_no: '%s'})-[k:KNOWS]-(friend)-[pt:PARTY_TO]->(c:Crime), (anne)-[ca1:CURRENT_ADDRESS]->(aAddress)-[lia1:LOCATION_IN_AREA]->(area), (friend)-[ca2:CURRENT_ADDRESS]->(fAddress)-[lia2:LOCATION_IN_AREA]->(area) RETURN *", dni);
-            return session.run(query).stream().collect(Collectors.toList());
+            return mapResponseToString(session, query);
         }
+    }
+
+    private String mapResponseToString(Session session, String query){
+        List<List<Pair<String, Value>>> list = new ArrayList<>();
+        for (List<Pair<String, Value>> pairs : session.run(query).list(a -> a.fields())) {
+            list.add(pairs);
+        }
+        String json = new Gson().toJson(list);
+        return json;
     }
 
 
